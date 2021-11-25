12533e99d4c3143b0979b8f1c4ace558c382720d
George <gzald001@gmail.com>
2021-11-25 11:11:18 -0500

    docs: Add comments

--- js/app.js
+++ js/app.js
@@ -13,114 +13,99 @@
  * 
 */
 
+// parse the DOM content before executing js code
+document.addEventListener("DOMContentLoaded", populateNavBar);
+
 /**
- * Comments should be present at the beginning of each procedure and class.
- * Great to have comments before crucial code sections within the procedure.
-*/
-
-
-document.addEventListener("DOMContentLoaded", main);
-
-function main(){
-    document.addEventListener("mouseover", activateSections);
-    populateNavBar();
-}
-
+ * @description Activates associated section when in viewport
+ */
 function activateSections(){
+
+    // get all sections
     let sections = document.querySelectorAll("section");
+
+    // for each section get viewport and check if section is in viewport
     for(const section of sections){
+
+        // get section dimensional coordinates
         let sectionDomRect = section.getBoundingClientRect();
-        let inViewPort = sectionDomRect.left >= 0 && sectionDomRect.top >= 0 
+
+        // check if section is in viewport
+        let inViewPort = sectionDomRect.left >= 0 && sectionDomRect.top >= 0
         && (sectionDomRect.right <= (window.innerWidth
         || document.documentElement.clientWidth))
         && (sectionDomRect.bottom <= (window.innerHeight
         || document.documentElement.clientHeight));
+
+        // if in viewport render section active for css styling
         if(inViewPort){
-            section.classList.add("your-active-class");
-        } else {
-            section.classList.remove("your-active-class");
+            section.classList.add("active"); 
+        } else
+            section.classList.remove("active");
         }
-    }
 }
 
+/**
+ * @description Builds navigation bar functionality
+ */
 function populateNavBar(){
+
+    // sectional css styling active upon scrolling down webpage
+    document.addEventListener("mouseover", activateSections);
+
+    // get div tags associated with section class landing container
     let parts = document.querySelectorAll(".landing__container");
+
+    // gets ul tag to append list items
     let ul = document.querySelector("#navbar__list");
+
+    // iterates through all sections of the webpage
     for(let i = 1; i <= parts.length; i++){
+        
+        // creates list item and a div tag
         let li = document.createElement("li");
         let div = document.createElement("div");
+
+        // adds the menu__link class for css styling
         div.classList.add("menu__link");
+
+        // then appends div to list item and list item to ul tag
         li.appendChild(div);
         ul.appendChild(li);
+
+        // changes cursor to a pointer over anchors
         div.style.cursor = "pointer";
+
+        // adds anchor section title content to nav bar
         div.textContent = `Section ${i}`; 
     }
 
+    // listener scrolls into associated section upon clicking
     ul.addEventListener("click", scrollIntoSection);
 }
 
+/**
+ * @description Event listener function scrolls into section upon clicking event
+ * @param {click} event 
+ */
+
 function scrollIntoSection(event){
+    
+    // checks if the event target is the div tag that contains the anchor
     if (event.target.tagName === "DIV"){
+
+        // gets the div content section and section number
         let section = event.target.textContent;
+
+        // gets the section number
         let sectionNum = section.charAt(section.length - 1);
+
+        // gets the section tag by section id
         let sectionView = document.getElementById(`section${sectionNum}`)
+
+        // scrolls into view to the appropriate section
         sectionView.scrollIntoView({behavior: "smooth"}); 
     }  
+
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/**
- * Define Global Variables
- * 
-*/
-
-
-
-/**
- * End Global Variables
- * Start Helper Functions
- * 
-*/
-
-
-
-/**
- * End Helper Functions
- * Begin Main Functions
- * 
-*/
-
-// build the nav
-
-
-// Add class 'active' to section when near top of viewport
-
-
-// Scroll to anchor ID using scrollTO event
-
-
-/**
- * End Main Functions
- * Begin Events
- * 
-*/
-
-// Build menu 
-
-// Scroll to section on link click
-
-// Set sections as active
-
 
